cf18c4b6c0b79abdf77149db9280091d635e0dd6
Polina Yukhymenko <polya.kiev@gmail.com>
2024-06-19 14:52:07 +0200

    done lab

--- index.js
+++ index.js
@@ -9,11 +9,9 @@ const s5 = "and";
 
 // Concatenate the string variables into one new string
 
-
 // Print out the concatenated string
-
-
-
+const tongueTwister = "${s1} ${s2} ${s3} ${s4} ${s5}";
+console.log(tongueTwister);
 
 /*******************************************
     Iteration 1.2 | Camel Tail
@@ -23,24 +21,26 @@ const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
 
-
 // Print the cameLtaiL-formatted string
 
+// Convert the last letter of part1 and part2 to uppercase
+const part1CameLtaiL = part1.slice(0, -1) + part1.slice(-1).toUpperCase();
+const part2CameLtaiL = part2.slice(0, -1) + part2.slice(-1).toUpperCase();
 
+// Concatenate the strings with "cameLtaiL" formatting
+const result = part1CameLtaiL + part2CameLtaiL;
 
-
+// Print the cameLtaiL-formatted string
+console.log(result);
 /*******************************************
     Iteration 2.1 | Calculate Tip
 *******************************************/
 const billTotal = 84;
-
+const tipAmount = billTotal * 1.5;
+console.log(tipAmount);
 // Calculate the tip (15% of the bill total)
 
-
 // Print out the tipAmount
-
-
-
 
 /*******************************************
     Iteration 2.2 | Generate Random Number
@@ -48,11 +48,9 @@ const billTotal = 84;
 
 // Generate a random integer between 1 and 10 (inclusive)
 
-
 // Print the generated random number
-
-
-
+const randomNumber = Math.floor(Math.random() * 10) + 1;
+console.log(randomNumber);
 /*******************************************
     Iteration 3.1 | Booleans
 *******************************************/
@@ -61,16 +59,23 @@ const a = true;
 const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
-const expression1 = a && b;
+const expression1 = a && b; //false
 
-const expression2 = a || b;
+const expression2 = a || b; //true
 
-const expression3 = !a && b;
+const expression3 = !a && b; //false
 
-const expression4 = !(a && b);
+const expression4 = !(a && b); //true
 
-const expression5 = !a || !b;
+const expression5 = !a || !b; //true
 
-const expression6 = !(a || b);
+const expression6 = !(a || b); //false
 
-const expression7 = a && a;
+const expression7 = a && a; //true
+console.log(expression1);
+console.log(expression2);
+console.log(expression3);
+console.log(expression4);
+console.log(expression5);
+console.log(expression6);
+console.log(expression7);
